2e98351f5f739ec175a79f084a96f775e24fbe4e
Cristiano <cristianovelkan@gmail.com>
2022-11-18 05:36:32 -0300

    value object id tests

---
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -0,0 +1,25 @@
+import InvalidUuidError from "../errors/invalid-uuid.error";
+import UniqueEntityId from "./unique-entity-id.vo";
+import { validate as uuidValidate } from "uuid";
+
+describe("UniqueEntityId Unit Tests", () => {
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+
+  it("should throw error when uuid is invalid", () => {
+    expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const uuid = "b7b3765b-911c-4715-813d-c13f6cda3bbf";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const vo = new UniqueEntityId();
+    expect(uuidValidate(vo.id)).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalled();
+  });
+});
