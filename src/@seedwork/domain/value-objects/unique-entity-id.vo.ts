057d2479a759d9d4c755954be7643b60ad953ac3
Cristiano <cristianovelkan@gmail.com>
2022-11-19 07:55:10 -0300

    abstracting value object

---
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -0,0 +1,18 @@
+import { v4 as uuidV4, validate as uuidValidate } from "uuid";
+import InvalidUuidError from "../../errors/invalid-uuid.error";
+import ValueObject from "./value-object";
+
+export default class UniqueEntityId extends ValueObject<string> {
+  
+  constructor(readonly id?: string) {
+    super(id || uuidV4());
+    this.validate();
+  }
+
+  private validate(): void {
+    const isValid = uuidValidate(this.id);
+    if (!isValid) {
+      throw new InvalidUuidError();
+    }
+  }
+}
